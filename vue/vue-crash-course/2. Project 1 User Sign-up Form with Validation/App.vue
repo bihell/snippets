b4d4c7e404a6dabc9292c9e54aaf4a6dcf5a26ca
tpxcer <tpxcer@outlook.com>
2020-11-03 10:02:38 +0800

    doc

---
+++ vue/vue-crash-course/2. Project 1 User Sign-up Form with Validation/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <my-button
+    color="white"
+    background="darkslateblue"
+    :disabled="!valid"
+  />
+</template>
+
+<script>
+import MyButton from "./MyButton.vue"
+export default {
+  components:{
+    MyButton
+  },
+  data(){
+    return{
+      valid:true
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
