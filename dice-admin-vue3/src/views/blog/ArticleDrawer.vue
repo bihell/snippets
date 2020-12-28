5a48779ff968ee2b0d77cfdcbed62f7ae05dd6e2
tpxcer <tpxcer@outlook.com>
2020-12-28 13:45:22 +0800

    chore: update

--- dice-admin-vue3/src/views/blog/ArticleDrawer.vue
+++ dice-admin-vue3/src/views/blog/ArticleDrawer.vue
@@ -1,21 +1,21 @@
 <template>
-  <BasicDrawer v-bind="$attrs" @register="register" title="Drawer Title" width="50%">
+  <BasicDrawer v-bind="$attrs" @register="register" title="Drawer Title" width="40%">
     <div>
       <BasicForm @register="registerForm" />
     </div>
+    <div class="bottom-control">
+      <a-button class="mb-2" type="dashed" @click="advancedVisible = true"> 高级 </a-button>
+      <a-space>
+        <a-button type="dashed" @click="advancedVisible = true"> 高级 </a-button>
+      </a-space>
+    </div>
   </BasicDrawer>
-  <PageFooter>
-    <template #right>
-      <a-button class="mr-2" type="primary" @click="submitAll"> 保存草稿 </a-button>
-      <a-button class="mr-2" type="danger" @click="submitAll"> 保存 </a-button>
-    </template>
-  </PageFooter>
 </template>
 <script lang="ts">
   import { defineComponent } from 'vue';
   import { BasicDrawer, useDrawerInner } from '/@/components/Drawer';
-
   import { BasicForm, FormSchema, useForm } from '/@/components/Form/index';
+
   const schemas: FormSchema[] = [
     {
       field: 'field1',
@@ -24,7 +24,7 @@
       colProps: {
         span: 12,
       },
-      defaultValue: '111',
+      defaultValue: '111222',
     },
     {
       field: 'field2',
@@ -57,3 +57,23 @@
     },
   });
 </script>
+
+<style lang="less" scoped>
+  .bottom-control {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    padding: 7px 14px;
+    text-align: right;
+    background: rgb(255, 255, 255);
+    border-top: 1px solid rgb(232, 232, 232);
+    border-radius: 0 0 4px 4px;
+    box-shadow: 0 -6px 16px -8px rgba(0, 0, 0, 0.08), 0 -9px 28px 0 rgba(0, 0, 0, 0.05),
+      0 -12px 48px 16px rgba(0, 0, 0, 0.03);
+  }
+
+  .mb-2 {
+    margin-bottom: 1em !important;
+  }
+</style>
