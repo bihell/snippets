73d73cdba16493c42d44f37858488274e071fbe3
tpxcer <tpxcer@outlook.com>
2021-01-18 15:16:59 +0800

    chore: update

--- dice-admin-vue3/src/views/test/sign-up/SignUp.vue
+++ dice-admin-vue3/src/views/test/sign-up/SignUp.vue
@@ -1,20 +1,22 @@
 <template>
-  <MyInput
-    name="Username"
-    :value="username.value"
-    :rules="{ required: true, min: 5 }"
-    type="text"
-    @update="update"
-  />
-  <MyInput
-    name="Password"
-    :value="password.value"
-    :rules="{ required: true, min: 10 }"
-    type="password"
-    @update="update"
-  />
-  <MyButton color="white" background="darkslateblue" :disabled="!valid" />
-  <!--  也可以是<my-button>-->
+  <form class="form" @submit.prevent="submit">
+    <MyInput
+      name="Username"
+      :value="username.value"
+      :rules="{ required: true, min: 5 }"
+      type="text"
+      @update="update"
+    />
+    <MyInput
+      name="Password"
+      :value="password.value"
+      :rules="{ required: true, min: 10 }"
+      type="password"
+      @update="update"
+    />
+    <MyButton color="white" background="darkslateblue" :disabled="!valid" />
+    <!--  也可以是<my-button>-->
+  </form>
 </template>
 
 <script>
@@ -40,8 +42,12 @@
       },
     },
     methods: {
+      submit() {
+        // submit($evt){
+        // $evt.preventDefault()
+        console.log('submit');
+      },
       update(payload) {
-        console.log(payload);
         this[payload.name.toLowerCase()] = {
           valid: payload.valid,
           value: payload.value,
@@ -54,5 +60,10 @@
 <style scoped>
   body {
     font-family: Arial;
+  }
+
+  .form {
+    width: 50%;
+    max-width: 400px;
   }
 </style>
