f3c284570c9014a09cf4dae258b53f13078e1281
tpxcer <tpxcer@outlook.com>
2021-01-19 14:35:25 +0800

    chore: update

--- dice-admin-vue3/src/views/test/pokemon/PokemonSolt.vue
+++ dice-admin-vue3/src/views/test/pokemon/PokemonSolt.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="cards">
-    <Card v-for="starter in starters">
+    <Card v-for="starter in starters" @click="fetchEvolutions(starter)">
       <template #title>
         {{ starter.name }}
       </template>
@@ -14,13 +14,29 @@
       </template>
     </Card>
   </div>
+
+  <div class="cards">
+    <Card v-for="creature in evolutions">
+      <template #title>
+        {{ creature.name }}
+      </template>
+      <template #content>
+        <img :src="creature.sprite" />
+      </template>
+      <template #description>
+        <div v-for="type in creature.types">
+          {{ type }}
+        </div>
+      </template>
+    </Card>
+  </div>
 </template>
 
 <script>
   import Card from './Card.vue';
 
   const api = 'https://pokeapi.co/api/v2/pokemon';
-  const ids = [1, 4, 7];
+  const STARTER_IDS = [1, 4, 7];
 
   export default {
     components: {
@@ -29,22 +45,29 @@
     data() {
       return {
         starters: [],
+        evolutions: [],
       };
     },
 
-    created() {
-      this.fetchData();
+    async created() {
+      this.starters = await this.fetchData(STARTER_IDS);
     },
 
     methods: {
-      async fetchData() {
+      async fetchEvolutions(pokemon) {
+        this.evolutions = await this.fetchData([pokemon.id + 1, pokemon.id + 2]);
+        console.log(this.evolutions);
+      },
+
+      async fetchData(ids) {
         // Promise.all 表示同时等待多个异步请求
         const responses = await Promise.all(ids.map((id) => window.fetch(`${api}/${id}`)));
         const data = await Promise.all(responses.map((res) => res.json()));
-        this.starters = data.map((datum) => ({
+        return data.map((datum) => ({
           name: datum.name,
           sprite: datum.sprites.other['official-artwork'].front_default,
           types: datum.types.map((type) => type.type.name),
+          id: datum.id,
         }));
       },
     },
