ed4b72936c26d349a877aaf26a3a699bd7b7a7b6
tpxcer <tpxcer@outlook.com>
2021-01-19 16:43:14 +0800

    chore: update

--- dice-admin-vue3/src/views/test/composition/1.vue
+++ dice-admin-vue3/src/views/test/composition/1.vue
@@ -2,6 +2,12 @@
   <button @click="increment">
     {{ count }}
   </button>
+  <button @click="increase('a')">
+    {{ numbers.a }}
+  </button>
+  <button @click="increase('b')">
+    {{ numbers.b }}
+  </button>
 </template>
 
 <script>
@@ -9,7 +15,18 @@
 
   export default {
     setup() {
+      // ref 比较适合单个值
       const count = ref(0);
+
+      // reactive 比较适合复杂值，对象之类
+      const numbers = reactive({
+        a: 0,
+        b: 0,
+      });
+
+      const increase = (num) => {
+        numbers[num] += 1;
+      };
       const increment = () => {
         count.value += 1;
       };
@@ -17,6 +34,8 @@
       return {
         increment,
         count,
+        numbers,
+        increase,
       };
     },
   };
