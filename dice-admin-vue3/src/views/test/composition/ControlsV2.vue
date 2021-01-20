9925715553e2e8a59b877eb5395835c94bae3c75
tpxcer <tpxcer@outlook.com>
2021-01-20 15:19:28 +0800

    chore: update

--- dice-admin-vue3/src/views/test/composition/ControlsV2.vue
+++ dice-admin-vue3/src/views/test/composition/ControlsV2.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-    <button>
-      {{ post.likes }}
-    </button>
+    <div class="like">
+      <button @click="like"> Like </button>
+      <div>
+        {{ post.likes }}
+      </div>
+    </div>
+
     <Hashtag v-for="tag in post.hashtags" :hashtag="tag" />
   </div>
 </template>
 
 <script>
   import Hashtag from './HashtagV2.vue';
+  import { store } from './StoreV2';
   export default {
     components: { Hashtag },
     props: {
@@ -16,7 +21,23 @@
         type: Object,
       },
     },
+
+    setup(props) {
+      const like = () => {
+        store.incrementLike(props.post);
+      };
+
+      return {
+        like,
+      };
+    },
   };
 </script>
 
-<style scoped></style>
+<style scoped>
+  .like {
+    display: flex;
+    justify-content: center;
+    margin: 10px;
+  }
+</style>
--- dice-admin-vue3/src/views/test/composition/StoreV2.js
+++ dice-admin-vue3/src/views/test/composition/StoreV2.js
@@ -12,5 +12,14 @@ class Store {
   setHashtag(tag) {
     this.state.currentHashtag = tag;
   }
+
+  incrementLike(post) {
+    const thePost = this.state.posts.find((x) => x.id === post.id);
+
+    if (!thePost) {
+      return;
+    }
+    thePost.likes += 1;
+  }
 }
 export const store = new Store();
