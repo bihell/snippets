4bd9d4d8b81d70939b211f69931f7c4414620754
tpxcer <tpxcer@outlook.com>
2021-04-01 19:54:36 +0800

    chore: update

--- dice-admin-vue3/src/components/Button/src/BasicButton.vue
+++ dice-admin-vue3/src/components/Button/src/BasicButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <Button v-bind="getBindValue" :class="[getColor, $attrs.class]">
+  <Button v-bind="getBindValue" :class="[getColor, $attrs.class]" @click="onClick">
     <template #default="data">
       <Icon :icon="preIcon" v-if="preIcon" :size="14" />
       <slot v-bind="data"></slot>
@@ -12,6 +12,7 @@
 
   import { Button } from 'ant-design-vue';
   import Icon from '/@/components/Icon';
+
   import { propTypes } from '/@/utils/propTypes';
 
   export default defineComponent({
@@ -25,6 +26,7 @@
       disabled: propTypes.bool,
       preIcon: propTypes.string,
       postIcon: propTypes.string,
+      onClick: propTypes.func,
     },
     setup(props, { attrs }) {
       const getColor = computed(() => {
