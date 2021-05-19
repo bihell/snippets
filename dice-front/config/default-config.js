7c7f4976c9279c602a38159f7448e7b38a2c9824
tpxcer <tpxcer@outlook.com>
2021-05-19 16:19:57 +0800

    dice

--- dice-front/config/default-config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  meta_title: 'BIHell',
-  meta_description: '专注于大数据处理技术',
-  meta_keywords: 'Hadoop, Java, Hive, Spring-Boot',
-  meta_author: 'tpxcer@outlook.com',
-  google_site_verification: '',
-  baidu_site_verification: ''
-}
